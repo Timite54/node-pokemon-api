be9a93cfa69cab519fe662325f1463be9b80678a
Timite54 <kadernaho@gmail.com>
2025-03-11 02:11:36 +0000

    CRUD complet de pokemons

--- helper.js
+++ helper.js
@@ -2,3 +2,14 @@ exports.success = (message, data) => {
     return {message, data}
 }
 
+exports.getPokeId = (pokemons) => {
+    // Transforme pokemons[] en un tableau d'id de pokemon
+    const pokemonsIds = pokemons.map(pokemon =>  pokemon.id)
+    // calculcul l'id le plus grand parmi les id existant
+    const maxId = pokemonsIds.reduce((a,b) => Math.max(a,b))
+    // const maxId = Math.max.apply(Math, pokmonsIds);
+    return maxId + 1
+}
+
+
+
--- main.js
+++ main.js
@@ -1,17 +1,25 @@
 const express = require('express');
 const  morgan = require('morgan');
+const favicon = require('serve-favicon');
 let pokemons = require('./mock-pokemon');
-const{success} = require('./helper');
+const{success, getPokeId} = require('./helper');
 
 const app = express();
 const port = 2000;
 
-app.use(morgan('dev'));
+
+app
+    .use(favicon(__dirname + '/favicon.ico'))
+    .use(morgan('dev'))
+    .use(express.json())
 
 app.get("/api/pokemons/:id", (req, res) => {
     const id = parseInt(req.params.id);
     const pokemon = pokemons.find((pokemon) => pokemon.id === id);
     const message = "Un pokemon a été touvé !"
+    if (!pokemon) {
+        res.json("Pokemon non touvé !")
+    }
     res.json(success(message, pokemon));
 })
 
@@ -19,7 +27,41 @@ app.get("/api/pokemons", (req, res) => {
     const allPokemons = pokemons
     const message = "La liste complète des pokemons"
     res.json(success(message, allPokemons));
+})
 
+app.post("/api/pokemons", (req, res) => {
+    const uniqueID =  getPokeId(pokemons)
+    const newPokemon = {
+        id : uniqueID,
+        ...req.body,
+        ...{created : new Date()}
+    }
+    pokemons.push(newPokemon);
+    const message = `Le pokemon ${newPokemon.name} a été crée avec succès`
+    res.json(success(message, pokemons));
+})
+
+app.put("/api/pokemons/:id", (req, res) => {
+    const pokemonID = parseInt(req.params.id);
+    const pokemonIndex = pokemons.findIndex((el) => el.id ===pokemonID);
+    if (pokemonIndex === -1){
+        res.json({message: "Pokemon could not be found."})
+    }
+    pokemons[pokemonIndex] ={
+        ...pokemons[pokemonIndex],
+        ...req.body
+    };
+    // pokemons.push(pokemons[pokemonIndex]);
+    const message = "Pokemons mis à jour"
+    res.json(success(message, pokemons));
+})
+
+app.delete("/api/pokemons/:id", (req, res) => {
+    const pokemonID = +req.params.id;
+    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === pokemonID);
+    pokemons.filter(pokemon => pokemon.id !== pokemonID)
+    const message = "Pokemon deleted."
+    res.json(success(message, pokemonDeleted));
 })
 
 app.listen(port, () => {
