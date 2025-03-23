2b7ea188415a21471bc007dbc3a53de1bf84daa1
Timite54 <101226879+Timite54@users.noreply.github.com>
2025-03-23 16:50:46 +0100

    changement de l'archi

--- main.js
+++ main.js
@@ -1,88 +1,18 @@
 const express = require("express");
 const  morgan = require('morgan');
-const favicon = require('serve-favicon');
-let pokemons = require('./mock-pokemon');
-const{success, getPokeId} = require('./helper');
-const {Sequelize, DataTypes} = require('sequelize')
-const PokemonModel = require('./src/models/Pokemon')
+const favicon = require('serve-favicon')
+const sequelize= require('./src/db/sequelize')
+
+
 const app = express();
 const port = 2000;
-
-const sequelize = new Sequelize('pokedex', 'root', '', {
-    host: 'localhost',
-    dialect: "mariadb",
-    logging: false,
-})
-
-sequelize.authenticate()
-    .then(_ =>
-    console.log('Connection has been established successfully.')
-).catch(error =>
-    console.error('Unable to connect to the database:', error)
-)
-PokemonModel(sequelize, DataTypes)
-sequelize.sync({alter: true})
-.then(_ =>
-    console.log('All models were synchronized successfully.')
-).catch(
-    error => console.log(error)
-)
 
 app
     .use(favicon(__dirname + '/favicon.ico'))
     .use(morgan('dev'))
     .use(express.json())
 
-app.get("/api/pokemons/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
-    const message = "Un pokemon a été touvé !"
-    if (!pokemon) {
-        res.json("Pokemon non touvé !")
-    }
-    res.json(success(message, pokemon));
-})
-
-app.get("/api/pokemons", (req, res) => {
-    const allPokemons = pokemons
-    const message = "La liste complète des pokemons"
-    res.json(success(message, allPokemons));
-})
-
-app.post("/api/pokemons", (req, res) => {
-    const uniqueID =  getPokeId(pokemons)
-    const newPokemon = {
-        id : uniqueID,
-        ...req.body,
-        ...{created : new Date()}
-    }
-    pokemons.push(newPokemon);
-    const message = `Le pokemon ${newPokemon.name} a été crée avec succès`
-    res.json(success(message, pokemons));
-})
-
-app.put("/api/pokemons/:id", (req, res) => {
-    const pokemonID = parseInt(req.params.id);
-    const pokemonIndex = pokemons.findIndex((el) => el.id ===pokemonID);
-    if (pokemonIndex === -1){
-        res.json({message: "Pokemon could not be found."})
-    }
-    pokemons[pokemonIndex] ={
-        ...pokemons[pokemonIndex],
-        ...req.body
-    };
-    // pokemons.push(pokemons[pokemonIndex]);
-    const message = "Pokemons mis à jour"
-    res.json(success(message, pokemons));
-})
-
-app.delete("/api/pokemons/:id", (req, res) => {
-    const pokemonID = +req.params.id;
-    const pokemonDeleted = pokemons.find(pokemon => pokemon.id === pokemonID);
-    pokemons.filter(pokemon => pokemon.id !== pokemonID)
-    const message = "Pokemon deleted."
-    res.json(success(message, pokemonDeleted));
-})
+sequelize.innitDB()
 
 app.listen(port, () => {
     console.log(`Listening on : http://localhost:${port}`);
---
+++ src/db/sequelize.js
@@ -0,0 +1,32 @@
+const {Sequelize, DataTypes} = require('sequelize')
+const PokemonModel = require('../models/Pokemon')
+const pokemons = require('./mock-pokemon')
+
+const sequelize = new Sequelize('pokedex', 'root', '', {
+    host: 'localhost',
+    dialect:'mariadb',
+    logging:false
+})
+
+const Pokemon = PokemonModel(sequelize, DataTypes)
+const innitDB = async () => {
+    await sequelize.authenticate()
+        .then(_ =>
+            console.log('Connection has been established successfully.')
+        ).catch(error =>console.error('Unable to connect to the database:', error))
+    await sequelize.sync({force: true})
+        .then(_ => {
+            console.log('All models were synchronized successfully.')
+            pokemons.map(pokemon => {
+                Pokemon.create({
+                    name: pokemon.name,
+                    hp: pokemon.hp,
+                    cp: pokemon.cp,
+                    picture: pokemon.picture,
+                    types: pokemon.types.join()
+                }).then(bulbizarre => console.log(bulbizarre.toJSON()))
+            })
+        }).catch(error => console.log(error))
+}
+
+module.exports = { innitDB, Pokemon}
